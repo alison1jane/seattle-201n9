1994148ccce6967e7f86fb497b72f2f1ed783e7d
Jane Smith <alison.mcirvin@gmail.com>
2017-10-07 18:37:52 -0700

    working on q3, still issues

--- class-05-images-color-text/lab/app.js
+++ class-05-images-color-text/lab/app.js
@@ -10,7 +10,7 @@ Test this function by hand in the console to get it working, and when you think 
 
 For TODO item, be sure to change it to say DONE when you have successfully completed the task*/
 
-// TODO: Write your code here
+///TODO: Write your code here
 function sum(a,b){ //eslint-disable-line
   var theSum = a + b;
   var message = 'The sum of ' + a + ' and ' + b + ' is ' + theSum + '.';
@@ -52,12 +52,22 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // TODO: Write your code here
-function sumAndMultiply(a,b,c){ //eslint-disable-line
-
+function sumAndMultiply(a,b,c) { //eslint-disable-line
+  var sum1 = sum(a, b);
+  console.log(sum1[0]);
+  var sum2 = sum(sum1[0], c);
+  console.log(sum2[0, 1]);
+  var message1 = 'The sum of ' + a + ' and ' + b + ' and ' + c + ' is ' + sum2 + '.';
+  var multiply1 = multiply(a,b,c);
+  console.log(multiply1[0]);
+  var multiply2 = sum(multiply1[0], c);
+  console.log(multiply2[0, 1]);
+  var message2 = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + multiply1 + '.';
+  return [sum, message1, multiply, message2];
 }
 
 // TODO: Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
